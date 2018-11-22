fixed one-hot encoding problem, added possibility to clip gradients.

mnist_standalone.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 from ncp.datasets.mnist import load_mnist
 
-def network(data, layer_sizes = [256, 256, 10]):
+def network(data, layer_sizes = [256, 256, 10], ncp_scale = 0.1):
     '''
     Defines network topology 
     '''
@@ -27,8 +27,8 @@ def network(data, layer_sizes = [256, 256, 10]):
             )
 
     #computes the ncp_loss, in this case simply the entropy, which we want to minimize over the out-of-distribution training data
-    class_probabilities = tf.nn.softmax(logits)
-    ncp_loss = tf.reduce_sum(-class_probabilities * tf.log(class_probabilities))
+    class_probabilities = tf.nn.softmax(logits * tf.constant(ncp_scale, dtype = tf.float32))
+    ncp_loss = tf.reduce_mean(-class_probabilities * tf.log(class_probabilities))
     return standard_loss, ncp_loss, logits, class_probabilities
 
 def generate_partial_mnist(digits_to_omit):
@@ -37,10 +37,18 @@ def generate_partial_mnist(digits_to_omit):
     removes the digits specified in the list (digits_to_omit).
     '''
     images, labels = load_mnist()
+
+    # Remove unwanted digits from training data
     for digit in digits_to_omit:
         indices = np.argwhere(labels == digit)
         labels = np.delete(labels, indices, axis = 0)
         images = np.delete(images, indices, axis = 0)
+
+    #remap the other digits to {0, .., n_digits}, to be able to do one_hot encoding
+    subtract_vector = np.zeros(10)
+    for digit in digits_to_omit:
+        subtract_vector[digit + 1:] += np.ones(10 - digit - 1)
+    labels -= subtract_vector[labels].astype(np.uint8) 
     return images, labels
 
 def generate_od_data(images, labels):
@@ -65,39 +73,49 @@ def get_batches(images, labels, batch_size):
 
 ###################
 # Some hyperparameters
-learning_rate = 0.001
-training_epochs = 1
+learning_rate = 0.0001
+training_epochs = 10
 batch_size = 100
 display_step = 1
-layer_sizes = [256, 256, 10]
-digits_to_omit = []
+digits_to_omit = [3, 4, 5, 6]
+output_layer_size = 10 - len(digits_to_omit)
+layer_sizes = [256, 256, output_layer_size]
+ncp_scale = 0.001 #scaling of logits output before the entropy is calculated, to reduce the size of the gradients
 alpha = 1 # weight factor between both contributions to the loss
+clip_at = (-1., 1.)
 ##################
 
 # PLACEHOLDERS FOR TRAINING DATA (id == in-distribution, od == out-of-distribution)
 id_images_ = tf.placeholder(tf.float32, [None, 28 * 28])
 id_labels_ = tf.placeholder(tf.int32, [None,])
+one_hot_ = tf.one_hot(id_labels_, output_layer_size)
 id_data = (
         id_images_,
-        tf.one_hot(id_labels_, 10)
+        tf.one_hot(id_labels_, output_layer_size)
            )
 
 od_images_ = tf.placeholder(tf.float32, [None, 28 * 28])
 od_labels_ = tf.placeholder(tf.int32, [None,])
 od_data = (
-        od_images_, tf.one_hot(od_labels_,
-        10 - len(digits_to_omit))
+        od_images_,
+        tf.one_hot(od_labels_, output_layer_size)
           )
 
 # need to specify template in order to ensure network variables are shared between id and od calculations
-network_tpl = tf.make_template('network', network, layer_sizes = layer_sizes)
+network_tpl = tf.make_template('network', network, layer_sizes = layer_sizes, ncp_scale = ncp_scale)
 id_loss, _, logits, _ = network_tpl(id_data) # calculate CE loss for id input data
 _, od_loss, logits_2, class_probabilities = network_tpl(od_data) # calculate entropy for od input data
 
 # loss function is sum of id and od contributions
-loss = alpha * id_loss + (1 - alpha) * od_loss
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-train_op = optimizer.minimize(loss)
+loss = alpha * id_loss #+ (1 - alpha) * od_loss
+#optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+#train_op = optimizer.minimize(loss)
+
+optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
+gvs = optimizer.compute_gradients(loss)
+capped_gvs = [(tf.clip_by_value(grad, clip_at[0], clip_at[1]), var) for grad, var in gvs]
+train_op = optimizer.apply_gradients(capped_gvs)
+
 # Initializing the variables
 init = tf.global_variables_initializer()
 
@@ -116,18 +134,27 @@ with tf.Session() as sess:
         total_batch = int(len(id_labels) / batch_size)
         # Loop over all batches
         for i in range(total_batch):
+        #for i in range(3):
             id_batch_images, id_batch_labels = next(id_batches)
             od_batch_images, od_batch_labels = next(od_batches)
             # Run optimization op (backprop) and cost op (to get loss value)
-            _, c, class_probs, logits_ = sess.run(
-                    [train_op, loss, class_probabilities, logits_2],
+            _, c, class_probs, logits_, od_loss_, one_hot = sess.run(
+                    [train_op, loss, class_probabilities, logits_2, od_loss, one_hot_],
                     feed_dict={id_images_: id_batch_images,
                                id_labels_: id_batch_labels,
                                od_images_: od_batch_images,
                                od_labels_: od_batch_labels})
             # Compute average loss
             avg_cost += c / total_batch
-        print(logits_[0])
+
+            #image = np.reshape(id_batch_images[1], (28, 28))
+            #print(id_batch_labels[1])
+            #plt.imshow(image)
+            #plt.show()
+            #print(one_hot[1])
+            #if i % 100 == 0: print('Output entropy: ', od_loss_)
+            #if i % 100 == 0: print('Loss: ', c)
+        #print(logits_[0])
         # Display logs per epoch step
         if epoch % display_step == 0:
             print("Epoch:", '%04d' % (epoch+1), "cost={:.9f}".format(avg_cost))
@@ -135,7 +162,7 @@ with tf.Session() as sess:
 
     # Test model
     pred = tf.nn.softmax(logits)  # Apply softmax to logits
-    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(tf.one_hot(id_labels_, 10), 1))
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(tf.one_hot(id_labels_, output_layer_size), 1))
     # Calculate accuracy
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
     print("Accuracy:", accuracy.eval({id_images_: id_images, id_labels_: id_labels}))