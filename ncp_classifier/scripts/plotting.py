42024e80c837a476c63fbdbdf67af2a2ff74c383
wasd12345 <uy65askvg23o987gli2ue97lu23i@gmail.com>
2018-11-27 10:06:21 -0500

    did alpha experiment w entropies as func of alpha

--- ncp_classifier/scripts/plotting.py
+++ ncp_classifier/scripts/plotting.py
@@ -19,7 +19,7 @@ plt.rcParams['pgf.texsystem'] = 'pdflatex'
 # PARAMETERS
 # =============================================================================
 MODE = 'ncp_on' #'ncp_off'
-EXPERIMENT_NAME = 'rotate'
+EXPERIMENT_NAME = 'alpha' #'alpha' #'rotate'
 
 
 
@@ -37,75 +37,168 @@ data_list = [os.path.join(logdir,i) for i in os.listdir(logdir) if i.endswith('.
 print(data_list)
 
 
-for dd in data_list:
+
+
+
+# =============================================================================
+# The "rotate" experiment:
+# For each distribution of allowed angles, look at the entropies over training
+# =============================================================================
+if EXPERIMENT_NAME=='rotate':
+    for dd in data_list:
+        
+        s = dd.find('__')+2
+        e = dd.find('.p')
+        exp = dd[s:e]
+        logging = pickle.load(open(dd, 'rb'), encoding='latin1')
+        
+        x_axis = range(len(logging['id_loss']))
+        markersize = 7
+        
+        fig = plt.figure(figsize = (6, 4))
+        ax0 = fig.add_subplot(111)
+        ax0.plot(
+                x_axis,
+                logging['om_ncp_loss'],
+                markersize = markersize,
+                markerfacecolor = 'r',
+                marker = '.',
+                markeredgecolor = 'black',
+                linewidth = 0.5,
+                linestyle = '--',
+                color = 'k',
+                markeredgewidth = 0.4,
+                label = 'OOD entropy'
+                )
+        ax0.plot(
+                x_axis,
+                logging['id_ncp_loss'],
+                markersize = markersize,
+                markerfacecolor = 'b',
+                marker = '.',
+                markeredgecolor = 'black',
+                linewidth = 0.5,
+                linestyle = '--',
+                color = 'k',
+                markeredgewidth = 0.4,
+                label = 'in-distribution entropy'
+                )
+        #ax0.yaxis.set_major_formatter(FormatStrFormatter('%g'))
+        ax0.xaxis.set_major_formatter(FormatStrFormatter('%g'))
+        ax0.get_yaxis().set_tick_params(which='both', direction='in')
+        ax0.get_xaxis().set_tick_params(which='both', direction='in')
+        ax0.set_ylabel('Entropy')
+        ax0.set_xlabel('Iterations')
+        ax0.legend()
+        fig.savefig(os.path.join(savedir,f'ncp_loss_{exp}.pgf'), bbox_inches = 'tight')
+        fig.savefig(os.path.join(savedir,f'ncp_loss_{exp}.pdf'), bbox_inches = 'tight')
+        
+        fig = plt.figure(figsize = (6, 4))
+        ax0 = fig.add_subplot(111)
+        ax0.plot(
+                x_axis,
+                logging['id_loss'],
+                markersize = markersize,
+                markerfacecolor = 'r',
+                marker = '.',
+                markeredgecolor = 'black',
+                linewidth = 0.5,
+                linestyle = '--',
+                color = 'k',
+                markeredgewidth = 0.4,
+                label = 'in-distribution loss'
+                )
+        #ax0.yaxis.set_major_formatter(FormatStrFormatter('%g'))
+        ax0.xaxis.set_major_formatter(FormatStrFormatter('%g'))
+        ax0.get_yaxis().set_tick_params(which='both', direction='in')
+        ax0.get_xaxis().set_tick_params(which='both', direction='in')
+        ax0.legend()
+        ax0.set_ylabel('Loss')
+        ax0.set_xlabel('Iterations')
+        fig.savefig(os.path.join(savedir,f'losses_{exp}.pgf'), bbox_inches = 'tight')
+        fig.savefig(os.path.join(savedir,f'losses_{exp}.pdf'), bbox_inches = 'tight')
     
-    s = dd.find('__')+2
-    e = dd.find('.p')
-    exp = dd[s:e]
     
-    logging = pickle.load(open(dd, 'rb'), encoding='latin1')
-    x_axis = range(len(logging['id_loss']))
+    
+
+
+
+# =============================================================================
+# The "alpha" loss experiment:
+# For each value of alpha, for each of the 3 dataset partitions (in, out, omitted),
+# look at the uncertainty over the entire partition, as a function of alpha.
+# =============================================================================
+if EXPERIMENT_NAME=='alpha':
+    
+    alphas = []
+    id_entropy_mean = []
+    id_entropy_std = []
+    od_entropy_mean = []
+    od_entropy_std = []
+    om_entropy_mean = []
+    om_entropy_std = []
+    for dd in data_list:
+        s = dd.find('__')+2
+        e = dd.find('.p')
+        exp = dd[s:e]
+        logging = pickle.load(open(dd, 'rb'), encoding='latin1')    
+        
+        alphas.extend([logging['alpha']])
+        id_entropy_mean.extend([logging['id_entropy_mean']])
+        id_entropy_std.extend([logging['id_entropy_std']])
+        od_entropy_mean.extend([logging['od_entropy_mean']])
+        od_entropy_std.extend([logging['od_entropy_std']])
+        #For om, we test on whole om set at every iteration, so just use last (fully trained) value [-1]
+        om_entropy_mean.extend([logging['om_ncp_loss'][-1]])
+        om_entropy_std.extend([logging['om_ncp_std'][-1]])
+
+
     markersize = 7
-    
     fig = plt.figure(figsize = (6, 4))
     ax0 = fig.add_subplot(111)
-    ax0.plot(
-            x_axis,
-            logging['om_ncp_loss'],
-            markersize = markersize,
-            markerfacecolor = 'r',
-            marker = '.',
-            markeredgecolor = 'black',
-            linewidth = 0.5,
-            linestyle = '--',
-            color = 'k',
-            markeredgewidth = 0.4,
-            label = 'OOD entropy'
-            )
-    ax0.plot(
-            x_axis,
-            logging['id_ncp_loss'],
+    #In-distribution entropy
+    ax0.semilogx(alphas, id_entropy_mean,
             markersize = markersize,
             markerfacecolor = 'b',
-            marker = '.',
             markeredgecolor = 'black',
-            linewidth = 0.5,
+            marker = '.',
             linestyle = '--',
             color = 'k',
-            markeredgewidth = 0.4,
-            label = 'in-distribution entropy'
+            label = 'In-distribution'
             )
-    #ax0.yaxis.set_major_formatter(FormatStrFormatter('%g'))
-    ax0.xaxis.set_major_formatter(FormatStrFormatter('%g'))
-    ax0.get_yaxis().set_tick_params(which='both', direction='in')
-    ax0.get_xaxis().set_tick_params(which='both', direction='in')
-    ax0.set_ylabel('Entropy')
-    ax0.set_xlabel('Iterations')
-    ax0.legend()
-    fig.savefig(os.path.join(savedir,f'ncp_loss_{exp}.pgf'), bbox_inches = 'tight')
-    fig.savefig(os.path.join(savedir,f'ncp_loss_{exp}.pdf'), bbox_inches = 'tight')
-    
-    fig = plt.figure(figsize = (6, 4))
-    ax0 = fig.add_subplot(111)
-    ax0.plot(
-            x_axis,
-            logging['id_loss'],
+    #Out-of-distribution entropy
+    ax0.semilogx(alphas, od_entropy_mean,
+            markersize = markersize,
+            markerfacecolor = 'g',
+            markeredgecolor = 'black',
+            marker = '.',
+            linestyle = '--',
+            color = 'k',
+            label = 'OOD'
+            )
+    #Omitted digit entropy
+    ax0.semilogx(alphas, om_entropy_mean,
             markersize = markersize,
             markerfacecolor = 'r',
-            marker = '.',
             markeredgecolor = 'black',
-            linewidth = 0.5,
+            marker = '.',
             linestyle = '--',
             color = 'k',
-            markeredgewidth = 0.4,
-            label = 'in-distribution loss'
+            label = 'Omitted digit'
             )
-    #ax0.yaxis.set_major_formatter(FormatStrFormatter('%g'))
+    
+    #1 SD Error bars:
+    opacity = .5
+    for nn, a in enumerate(alphas):
+        plt.vlines(a, id_entropy_mean[nn] - id_entropy_std[nn], id_entropy_mean[nn] + id_entropy_std[nn], colors='b', alpha=opacity)
+        plt.vlines(a, od_entropy_mean[nn] - od_entropy_std[nn], od_entropy_mean[nn] + od_entropy_std[nn], colors='g', alpha=opacity)
+        plt.vlines(a, om_entropy_mean[nn] - om_entropy_std[nn], om_entropy_mean[nn] + om_entropy_std[nn], colors='r', alpha=opacity)
+                   
     ax0.xaxis.set_major_formatter(FormatStrFormatter('%g'))
     ax0.get_yaxis().set_tick_params(which='both', direction='in')
     ax0.get_xaxis().set_tick_params(which='both', direction='in')
     ax0.legend()
-    ax0.set_ylabel('Loss')
-    ax0.set_xlabel('Iterations')
+    ax0.set_ylabel('Entropy')
+    ax0.set_xlabel('alpha')
     fig.savefig(os.path.join(savedir,f'losses_{exp}.pgf'), bbox_inches = 'tight')
-    fig.savefig(os.path.join(savedir,f'losses_{exp}.pdf'), bbox_inches = 'tight')
+    fig.savefig(os.path.join(savedir,f'losses_{exp}.pdf'), bbox_inches = 'tight')    
