7fbdca2b9c0be90f41793f2066ae134aef92e423
sandvdnh <sv2563@columbia.edu>
2018-11-27 22:21:30 -0500

    some style corrections

--- ncp_classifier/datasets/mnist.py
+++ ncp_classifier/datasets/mnist.py
@@ -5,43 +5,63 @@ import pickle
 import os
 
 filename = [
-["training_images","train-images-idx3-ubyte.gz"],
-["test_images","t10k-images-idx3-ubyte.gz"],
-["training_labels","train-labels-idx1-ubyte.gz"],
-["test_labels","t10k-labels-idx1-ubyte.gz"]
-]
+    ["training_images", "train-images-idx3-ubyte.gz"],
+    ["test_images", "t10k-images-idx3-ubyte.gz"],
+    ["training_labels", "train-labels-idx1-ubyte.gz"],
+    ["test_labels", "t10k-labels-idx1-ubyte.gz"]
+    ]
+
 
 def download_mnist():
     base_url = "http://yann.lecun.com/exdb/mnist/"
     for name in filename:
-        print("Downloading "+name[1]+"...")
-        request.urlretrieve(base_url+name[1], os.path.join('ncp_classifier','datasets',name[1]))
+        print("Downloading " + name[1] + "...")
+        request.urlretrieve(
+                base_url + name[1],
+                os.path.join('ncp_classifier', 'datasets', name[1])
+                )
     print("Download complete.")
+
 
 def save_mnist():
     mnist = {}
     for name in filename[:2]:
-        with gzip.open(os.path.join('ncp_classifier','datasets',name[1]), 'rb') as f:
-            mnist[name[0]] = np.frombuffer(f.read(), np.uint8, offset=16).reshape(-1,28*28)
+        with gzip.open(
+                os.path.join(
+                    'ncp_classifier',
+                    'datasets',
+                    name[1]
+                    ), 'rb') as f:
+            mnist[name[0]] = np.frombuffer(
+                    f.read(),
+                    np.uint8,
+                    offset=16).reshape(-1, 28 * 28)
     for name in filename[-2:]:
-        with gzip.open(os.path.join('ncp_classifier','datasets',name[1]), 'rb') as f:
+        with gzip.open(
+                os.path.join(
+                    'ncp_classifier',
+                    'datasets',
+                    name[1]), 'rb') as f:
             mnist[name[0]] = np.frombuffer(f.read(), np.uint8, offset=8)
     with open("./ncp_classifier/datasets/mnist.pkl", 'wb') as f:
-        pickle.dump(mnist,f)
+        pickle.dump(mnist, f)
     print("Save complete.")
+
 
 def init():
     download_mnist()
     save_mnist()
 
+
 def load_mnist():
-    #download_mnist()
-    #save_mnist()
-    with open("./ncp_classifier/datasets/mnist.pkl",'rb') as f:
+    # download_mnist()
+    # save_mnist()
+    with open("./ncp_classifier/datasets/mnist.pkl", 'rb') as f:
         mnist = pickle.load(f)
     training_images = mnist['training_images']
     training_labels = mnist['training_labels']
     return training_images, training_labels
+
 
 if __name__ == '__main__':
     init()
